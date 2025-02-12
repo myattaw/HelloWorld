188bd0287ad1fab54fd39694bc162ebdf49b0b63
Michael <whitefoxpvp@gmail.com>
2025-02-11 19:10:34 -0500

    Added display the current date to the user.

--- src/main/java/me/yattaw/project/Main.java
+++ src/main/java/me/yattaw/project/Main.java
@@ -1,12 +1,26 @@
 package me.yattaw.project;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Hello and welcome!\n");
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+
+        // Ensure you are using LocalDateTime here
+        LocalDateTime now = LocalDateTime.now();
+
+        // Format the current date and time
+        String formattedDate = now.format(formatter);
+        System.out.println("Current Time: " + formattedDate);
 
     }
 }
